da400e7f772c17ab3c07b1f0ee92fb42f6df4094
xoyoon2 <taeyoon01@kaist.ac.kr>
2021-09-06 03:54:27 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -2,7 +2,7 @@ const API_KEY = "2d340cb23fed41f149041fdf38b00fab";
 function onGeoSuccess(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
