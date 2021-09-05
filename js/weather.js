b6bb0c114c396f016877a8ea0cf92bdc72f8a3f7
xoyoon2 <taeyoon01@kaist.ac.kr>
2021-09-06 02:55:31 +0900

    Add weather

---
+++ js/weather.js
@@ -0,0 +1,19 @@
+const API_KEY = "2d340cb23fed41f149041fdf38b00fab";
+function onGeoSuccess(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector(".weather__city");
+      city.innerText = data.name;
+      const weather = document.querySelector(".weather__weather");
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
